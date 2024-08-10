a79e8feb761196f3fed268806d1b656422f2e457
KancharodMeerat <151113760+KancharodMeerat@users.noreply.github.com>
2024-08-10 16:17:25 +0700

    lab

---
+++ react-app/src/features/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const Container = ({children}) => {
+    return <main>{children}</main>;
+}
+
+export default Container;
---
+++ react-app/src/features/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+      <header>
+        <a href="/" className="brand">
+          Trendie
+        </a>
+        <a href="/create-product">Create product</a>
+      </header>
+    );
+  }
+  export default Navbar;
